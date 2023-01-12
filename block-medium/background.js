efb1989574d1b2d7508bce6e42b66fd8f68464b0
Aline Guimarães <alinerguio@gmail.com>
2023-01-12 16:46:40 -0300

    Update background.js

--- block-medium/background.js
+++ block-medium/background.js
@@ -1,10 +1,11 @@
 chrome((tab) => {
     if (tab.url.includes("medium.com")) {
-        document.getElementsByClassName("kv")[0].style.display = 'none';
+        document.getElementsByClassName("kv")[0].style.display = 'none'; // hide feed
     }
   });
 
-//   document.getElementsByClassName("ls")[1].style.display = 'none';
-// document.getElementsByClassName("ls")[3].style.display = 'none';
-// document.getElementsByClassName("ls")[4].style.display = 'none';
-// document.getElementsByClassName("ls")[5].style.display = 'none';
+// document.getElementsByClassName("ls")[1].style.display = 'none'; // hide staff picks
+// document.getElementsByClassName("ls")[3].style.display = 'none'; // hide connect twitter
+// document.getElementsByClassName("ls")[3].style.display = 'none'; // hide recommended topics
+// document.getElementsByClassName("ls")[4].style.display = 'none'; // hide who to follow
+// document.getElementsByClassName("ls")[5].style.display = 'none'; // hide recently saved
